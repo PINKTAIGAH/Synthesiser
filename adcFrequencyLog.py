d39733de2749ec29a6f07d175189364ece6138cd
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2022-11-26 17:48:36 +0000

    added more frequnecy varIation datasets

--- adcFrequencyLog.py
+++ adcFrequencyLog.py
@@ -8,14 +8,18 @@ from time import sleep
 
 def main():
 	frequencyArray= []
-	while True:
-		adc= adcSignal(1)
-		frequencyArray.append(adc.adcPoll()[0])
-		sleep(0.01)
-		if len(frequencyArray) >5000:
-			break
-	frequencyArray= np.array([frequencyArray])
-	np.savetxt('frequencyVariation.txt', frequencyArray, delimiter= ',')
-	print(frequencyArray.size)
+	print(f'0/5 data sets completed')
+	for i in range(5):
+		singleRun= []
+		while True:
+			adc= adcSignal(1)
+			singleRun.append(adc.adcPoll()[0])
+			sleep(0.01)
+			if len(singleRun) >2500:
+				break
+		frequencyArray.append(singleRun)
+		print(f'{i+1}/5 data sets completed')
+	frequencyArray= np.array(frequencyArray)
+	np.savetxt('frequencyVariationData/8000HzVariation.txt', frequencyArray, delimiter= ',')
 if __name__== '__main__':
 	main()
