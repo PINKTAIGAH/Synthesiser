4292650e2238d3cf5711c54fdc2ffd50d5b6a5b3
PINKTAIGAH <giorgio.b.campanella@gmail.com>
2022-12-05 17:02:59 +0000

    fix labels on plots

--- generateSignalTest.py
+++ generateSignalTest.py
@@ -17,14 +17,14 @@ def main():
 	print(t_f-t_i)
 	
 	# Generate a second buffer
-	sawtooth_buffer= generate.createBuffer(4, 0.5, 1, f'triangle')
+	triangle_buffer= generate.createBuffer(4, 0.5, 1, f'triangle')
 
 	# Generate a third buffer
 	noise_buffer= generate.createBuffer(None, 0.05, 1, f'noise')
 	
 	# Plot the signals 
-	plt.plot(generate.t, square_buffer.T[0][:], label= 'sine wave @ 50 Hz')
-	plt.plot(generate.t, sawtooth_buffer.T[0][:], label= 'sawtooth wave @ 4 Hz')
+	plt.plot(generate.t, square_buffer.T[0][:], label= 'square wave @ 50 Hz')
+	plt.plot(generate.t, triangle_buffer.T[0][:], label= 'triangle wave @ 4 Hz')
 	plt.plot(generate.t, noise_buffer.T[0][:], label='noise signal' )
 	plt.xlabel('time, t (s)')
 	plt.ylabel('Frequency, f (Hz)', x= 0.5)
